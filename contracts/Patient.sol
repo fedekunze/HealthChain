28815933482c9902bfdc34baece10dc148eac6be
fedekunze <fekunze@berkeley.edu>
2017-12-08 09:38:08 -0800

    bugs updates

--- contracts/Patient.sol
+++ contracts/Patient.sol
@@ -11,21 +11,22 @@ contract Patient {
 
   struct UpdateData {
     address updater;
-    uint numberOfUpdates;
     uint timestamp;
     string jsonHash;
     string ipfs2Hash;
   }
 
   struct User {
-    address addr;
-    string transformed; // hash that allows to store and retrieve data
+    /*string transformed; // hash that allows to store and retrieve data*/
+    AccessData[] dataAccesses;
+    UpdateData[] dataUpdates;
+    uint numberOfUpdates;
+    uint numberOfAccesses;
   }
 
-  mapping (string => address) private patients; // index fingerprint to user
+  mapping (string => address) private indexToAddr; // index fingerprint to user
   mapping (address => string) private patientToFunc;
-  mapping (address => AccessData[]) public dataAccesses;
-  mapping (address => UpdateData[]) public dataUpdates;
+  mapping (address => User) public users;
 
   event patientCreated();
   event dataUpdated(address patient);
@@ -37,7 +38,6 @@ contract Patient {
     _;
   }
 
-
 //constructor
   function Patient() public {
     owner = msg.sender; // patient owner of the contract ?
@@ -45,37 +45,37 @@ contract Patient {
   }
 
   function userExists(string indexPrint) constant public returns (bool) {
-    if (patients[indexPrint] == address(0)) return false;
+    if (indexToAddr[indexPrint] == address(0)) return false;
     else return true;
   }
 
   function createPatient(address _addr, string _indexFingerprint, string _func) public {
-    patients[_indexFingerprint] = _addr;
+    indexToAddr[_indexFingerprint] = _addr;
     patientToFunc[_addr] = _func;
+    users[_addr].numberOfUpdates = 0;
+    users[_addr].numberOfAccesses = 0;
     patientCreated();
   }
 
   function accessData(address _requester, string _indexFingerprint) constant public returns (string) {
     uint time = now;
-    address patientAddr = patients[_indexFingerprint];
-    dataAccesses[patientAddr].push(AccessData({
+    address patientAddr = indexToAddr[_indexFingerprint];
+    users[patientAddr].dataAccesses[users[patientAddr].numberOfAccesses + 1] = AccessData({
       requester: _requester,
       timestamp: time
-      }));
+      });
     dataAccessed(_requester, patientAddr);
     string storage func = patientToFunc[patientAddr];
     return func;
   }
 
-  function updateData(address _pcp, string _ipfshashHash, string _jsonHash) ownlerOnly() public {
+  function updateData(address _patientAdrr, string _ipfshashHash, string _jsonHash) ownlerOnly() public {
     uint time = now;
-    uint updates = dataUpdates[_pcp][dataUpdates[_pcp].length -1].numberOfUpdates;
-    dataUpdates[_pcp].push(UpdateData({
-      updater: _pcp,
-      numberOfUpdates: updates + 1,
+    users[_patientAdrr].dataUpdates[users[_patientAdrr].numberOfUpdates + 1] = UpdateData({
+      updater: _patientAdrr,
       timestamp: time,
       jsonHash: _jsonHash,
-      ipfs2Hash: _ipfshashHash}));
-    dataUpdated(_pcp);
+      ipfs2Hash: _ipfshashHash});
+    dataUpdated(_patientAdrr);
   }
 }
